c20e7084a01ada407867b19b1f5bb00029c68820
Gary <monkey_man2k@hotmail.com>
2014-05-08 23:44:03 -0400

    More Stuff

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-import sys
+from aptdaemon import loop
+import sys, time, thread
 import gst
+import pygst
 import urllib2
 import ast
 import random
 from PyQt4 import QtGui
+import sys, os, time, thread
+import glib, gobject
+import pygst
+import gst
 
 # http://gstreamer.freedesktop.org/documentation/
 # http://gstreamer.freedesktop.org/data/doc/gstreamer/head/gst-plugins-base-plugins/html/gst-plugins-base-plugins-playbin.html
@@ -12,6 +18,7 @@ from PyQt4 import QtGui
 # http://stackoverflow.com/questions/20460296/playing-remote-audio-files-in-python
 #http://pygstdocs.berlios.de/pygst-reference/class-gstelement.html
 
+
 class PoolSidePlayer(QtGui.QWidget):
 
     def __init__(self):
@@ -19,10 +26,25 @@ class PoolSidePlayer(QtGui.QWidget):
 
         self.tracks = self.get_all_tracks()
 
-        #creates a playbin (plays media form an uri)
-        self.player = gst.element_factory_make("playbin", "player")
+        self.player = gst.element_factory_make("playbin2", "player")
+        fakesink = gst.element_factory_make("fakesink", "fakesink")
+        self.player.set_property("video-sink", fakesink)
+        bus = self.player.get_bus()
+        bus.add_signal_watch()
+        bus.connect("message", self.on_message)
 
         self.initUI()
+
+    def on_message(self, bus, message):
+        t = message.type
+        if t == gst.MESSAGE_EOS:
+            self.player.set_state(gst.STATE_NULL)
+            self.playmode = False
+        elif t == gst.MESSAGE_ERROR:
+            self.player.set_state(gst.STATE_NULL)
+            err, debug = message.parse_error()
+            print "Error: %s" % err, debug
+            self.playmode = False
 
     @staticmethod
     def get_all_tracks():
@@ -30,22 +52,23 @@ class PoolSidePlayer(QtGui.QWidget):
         tracks = ast.literal_eval(response.read())
         return tracks
 
-    def get_random_track(self):
-        return random.choice(self.tracks)
+    def shuffle_tracks(self):
+        self.tracks = self.get_all_tracks()
+        random.shuffle(self.tracks)
 
-    def play_track(self):
-        curr_track = self.get_random_track()
+    def play_tracks(self):
+        self.shuffle_tracks()
+        for curr_track in self.tracks:
+            self.playmode = True
+            self.player.set_property("uri", 'http://api.soundcloud.com/tracks/%s/stream?client_id=e72237107739281ffceb867534efd87c' \
+                           % curr_track['scId'])
+            self.player.set_state(gst.STATE_PLAYING)
+            while self.playmode:
+                time.sleep(1)
 
-        #our stream to play
-        #http://api.soundcloud.com/tracks/{id}/stream?client_id={YOUR_CLIENT_ID}
-        music_stream_uri = 'http://api.soundcloud.com/tracks/%s/stream?client_id=e72237107739281ffceb867534efd87c' \
-                           % curr_track['scId']
+        time.sleep(1)
+        loop.quit()
 
-        #set the uri
-        self.player.set_property('uri', music_stream_uri)
-
-        #start playing
-        self.player.set_state(gst.STATE_PLAYING)
 
     def play(self):
         if self.player.get_state()[1] == gst.STATE_PLAYING:
@@ -53,11 +76,14 @@ class PoolSidePlayer(QtGui.QWidget):
         elif self.player.get_state()[1] == gst.STATE_PAUSED:
             self.player.set_state(gst.STATE_PLAYING)
         else:
-            self.play_track()
+            thread.start_new_thread(self.play_tracks, ())
+            gobject.threads_init()
+            loop = glib.MainLoop()
+            loop.run()
 
     def skip(self):
         self.player.set_state(gst.STATE_NULL)
-        self.play_track()
+        self.play()
 
     def initUI(self):
 
